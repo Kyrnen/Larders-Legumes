d6cc62fbc8fda38141ec186f87febc56ce9ecfb0
nullitaire <68204434+nullitaire@users.noreply.github.com>
2021-04-04 15:52:53 -0400

    Dungeon Generator finished
    Finished Dungeon Generator

--- Larders & Legumes/Assets/Scripts/DungeonCreator scripts/RoomGenerator.cs
+++ Larders & Legumes/Assets/Scripts/DungeonCreator scripts/RoomGenerator.cs
@@ -1,26 +1,38 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using UnityEngine;
-//internal class RoomGenerator
-//{
-//    private int maxInterations;
-//    private int roomLengthMin;
-//    private int roomWidthMin;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
-//    public RoomGenerator(int maxInterations, int roomLengthMin, int roomWidthMin)
-//    {
-//        this.maxInterations = maxInterations;
-//        this.roomLengthMin = roomLengthMin;
-//        this.roomWidthMin = roomWidthMin;
-//    }
+public class RoomGenerator
+{
+    private int maxIterations;
+    private int roomLengthMin;
+    private int roomWidthMin;
 
-//    public List<RoomNode> GenerateRoomsInAGivenSpaces(List<Node> roomSpaces)
-//    {
-//        List<RoomNode> listToReturn = new List<RoomNode>();
-//        foreach (var space in roomSpaces)
-//        {
-//            Vector2Int newBottomLeftPoint = StructureHelper.GenerateBottomLeftCornerBetween(
-//                space.BottomLeftAreaCorner, space.TopRightAreaCorner, 0.1f, 1);
-//        }
-//    }
-//}
+    public RoomGenerator(int maxIterations, int roomLengthMin, int roomWidthMin)
+    {
+        this.maxIterations = maxIterations;
+        this.roomLengthMin = roomLengthMin;
+        this.roomWidthMin = roomWidthMin;
+    }
+
+    public List<RoomNode> GenerateRoomsInGivenSpaces(List<Node> roomSpaces, float roomBottomCornerModifier, float roomTopCornerModifier, int roomOffset)
+    {
+        List<RoomNode> listToReturn = new List<RoomNode>();
+        foreach (var space in roomSpaces)
+        {
+            Vector2Int newBottomLeftPoint = StructureHelper.GenerateBottomLeftCornerBetween(
+               space.BottomLeftAreaCorner, space.TopRightAreaCorner, roomBottomCornerModifier, roomOffset);
+
+            Vector2Int newTopRightPoint = StructureHelper.GenerateTopRightCornerBetween(
+               space.BottomLeftAreaCorner, space.TopRightAreaCorner, roomTopCornerModifier, roomOffset);
+               space.BottomLeftAreaCorner = newBottomLeftPoint;
+               space.TopRightAreaCorner = newTopRightPoint;
+               space.BottomRightAreaCorner = new Vector2Int(newTopRightPoint.x, newBottomLeftPoint.y);
+               space.TopLeftAreaCorner = new Vector2Int(newBottomLeftPoint.x, newTopRightPoint.y);
+            
+               listToReturn.Add((RoomNode)space);
+
+        }
+        return listToReturn;
+    }
+}
