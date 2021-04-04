0a8026e8e69d1f0f1b2085fb05395b37138571ef
Angela Lam <angelakam.lam@gmail.com>
2021-04-04 09:42:51 -0700

    Implement Enemy Health and Rotation
    Implemented Enemy HP UI and rotation so that enemy will always be facing the enemy.

--- Larders & Legumes/Assets/Scripts/Enemy/Enemy.cs
+++ Larders & Legumes/Assets/Scripts/Enemy/Enemy.cs
@@ -10,11 +10,43 @@ public class Enemy : MonoBehaviour
     int maxHealth = 25;
     int currentHealth;
 
+    public Transform cam;
+
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         currentHealth = maxHealth;
+    }
 
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + cam.forward);
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        health.SetValueTo(currentHealth);
+
+        if (health.GetCurrentValue() <= 0)
+        {
+            FindObjectOfType<GameManager>().PlayerDied();
+        }
+    }
+
+    public void Heal(int value)
+    {
+        if (currentHealth < maxHealth)
+        {
+            if (currentHealth + value <= maxHealth)
+                currentHealth += value;
+            else
+                currentHealth = maxHealth;
+
+            health.SetValueTo(currentHealth);
+        }
+        else
+            Debug.Log("You're at max health");
     }
 
     //Enemy should: Constantly face the player after spawning
