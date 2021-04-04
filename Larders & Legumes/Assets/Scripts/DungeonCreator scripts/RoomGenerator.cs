ad8ff4c3698916ceb6736cf7092986d2de4a8382
nullitaire <68204434+nullitaire@users.noreply.github.com>
2021-04-04 00:14:17 -0400

    4/3 final push
    fixed some syntax and added StructureHelper.cs

---
+++ Larders & Legumes/Assets/Scripts/DungeonCreator scripts/RoomGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+internal class RoomGenerator
+{
+    private int maxInterations;
+    private int roomLengthMin;
+    private int roomWidthMin;
+
+    public RoomGenerator(int maxInterations, int roomLengthMin, int roomWidthMin)
+    {
+        this.maxInterations = maxInterations;
+        this.roomLengthMin = roomLengthMin;
+        this.roomWidthMin = roomWidthMin;
+    }
+
+    public List<RoomNode> GenerateRoomsInAGivenSpaces(List<Node> roomSpaces)
+    {
+        List<RoomNode> listToReturn = new List<RoomNode>();
+        foreach (var space in roomSpaces)
+        {
+            Vector2Int newBottomLeftPoint = StructureHelper.GenerateBottomLeftCornerBetween(
+                space.BottomLeftAreaCorner, space.TopRightAreaCorner, 0.1f, 1);
+        }
+    }
+}
