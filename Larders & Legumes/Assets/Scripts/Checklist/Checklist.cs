28b781e9da13a09258f433eb0626bb446803401d
Angela Lam <angelakam.lam@gmail.com>
2021-04-06 01:07:17 -0700

    Generate Requests of varying size
    Implemented Requests that will be displayed on checklist.

--- Larders & Legumes/Assets/Scripts/Checklist/Checklist.cs
+++ Larders & Legumes/Assets/Scripts/Checklist/Checklist.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class Checklist : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Inventory inventory;
+    Dictionary<string, int> request;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+
+        request = Requests.DebugRequest(Requests.potentialItems);
+        //foreach (KeyValuePair<string, int> kvp in request)
+        //{
+        //    //textBox3.Text += ("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+        //    Debug.Log(string.Format("Key = {0}, Value = {1}", kvp.Key, kvp.Value));
+        //}
     }
 }
